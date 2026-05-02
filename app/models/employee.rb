d7cbd45521c1fd39cdb4b31e722b83babf257b87
Rakshit <rakshitdhingra@Yashs-MacBook-Air.local>
2026-05-02 13:55:22 +0530

    feat: add country validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ApplicationRecord
-  validates :full_name, presence: true
+  validates :full_name, :country, presence: true
   validates :salary, numericality: { greater_than: 0 }
 end
