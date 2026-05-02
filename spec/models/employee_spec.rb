0a7d460e5030d3e7098fa27a0d3676def56e6fa3
Rakshit <rakshitdhingra@Yashs-MacBook-Air.local>
2026-05-02 13:54:49 +0530

    test: validate presence of country

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe Employee, type: :model do
     employee = Employee.new(full_name: "Test", salary: -10)
     expect(employee).to_not be_valid
   end
+
+  it "is invalid without country" do
+    employee = Employee.new(full_name: "Test", country: nil)
+    expect(employee).to_not be_valid
+  end
 end
